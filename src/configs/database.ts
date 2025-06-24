98e62b6528ec80028feae7001821c3a92a11dcc7
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-06-24 17:17:56 +0200

    Add comments

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -9,11 +9,11 @@ export const sequelize = new Sequelize(database, username, password, configs);
 export async function initializeDatabase() {
   try {
     await sequelize.authenticate();
-    // Comment out the next line if you don't want to alter the tables automatically
-    // This is useful for development but should be used with caution in production
-    // If you want to reset the database, you can use { force: true } instead
-    // await sequelize.sync({ force: true });
-    await sequelize.sync({ alter: true }); // or { force: true } for dev
+    // For development, you can use sync to create tables based on models
+    // Uncomment the line below to sync models with the database
+    // Note: Create migrations for production use instead of sync
+    // This will create tables if they do not exist, or alter them if they do
+    // await sequelize.sync({ alter: true }); // or { force: true } for dev
     console.log('Database connected!');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
